890293661c542ca4e7a36af6732f2564634dd92b
anhqui <anhqui@gmail.com>
2024-05-12 16:49:00 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux"
+import { CartIcon } from "../icons"
+
+const Navbar = () => {
+const {amount} = useSelector(state => state.cart.amount)
+
+console.log(amount)
+
+  return (
+    <nav>
+        <div className="nav-center">
+            <h3>Phone Store</h3>
+            <div className="nav-container">
+                <CartIcon />
+                <div className="amount-container">
+                    <p className="total-amount">0</p>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
